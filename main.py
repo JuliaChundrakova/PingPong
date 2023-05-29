6f1567def80a54eb210e1b779df4f7d09281f567
JuliaChundrakova <134778146+JuliaChundrakova@users.noreply.github.com>
2023-05-29 18:32:06 +0300

    Update main.py
    added new functions

--- main.py
+++ main.py
@@ -16,3 +16,34 @@ win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
 window.fill(back)
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+
+racket1 = Player("raketka.png", 30, 200, 4, 50, 150)
+racket2 = Player("raketka.png", 520, 200, 4, 50, 150)
+ball = GameSprite("ball.png", 200, 200, 4, 50, 50)
+
+speed_x = 3
+speed_y = 3
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+
+    display.update()
+    clock.tick(FPS)  
