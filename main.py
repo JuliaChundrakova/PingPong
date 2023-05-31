bc199319edf856f3c5185f72f6e8331d17ee32ef
JuliaChundrakova <134778146+JuliaChundrakova@users.noreply.github.com>
2023-05-31 16:53:04 +0300

    Update main.py
    added new functions

--- main.py
+++ main.py
@@ -26,6 +26,10 @@ racket1 = Player("raketka.png", 30, 200, 4, 50, 150)
 racket2 = Player("raketka.png", 520, 200, 4, 50, 150)
 ball = GameSprite("ball.png", 200, 200, 4, 50, 50)
 
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 speed_x = 3
 speed_y = 3
 
@@ -40,6 +44,22 @@ while game:
         racket2.update_r()
         ball.rect.x += speed_x
         ball.rect.y += speed_y
+        
+         if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+
+        if ball.rect.y > win_height - 50 or ball.rect.y < 0:
+            speed_y *= -1 
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+
 
         racket1.reset()
         racket2.reset()
